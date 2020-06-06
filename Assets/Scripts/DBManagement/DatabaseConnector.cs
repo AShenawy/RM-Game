ac88f8f1d6c63d5588a6907c0b78c315c61eef1c
SinanEmiroglu <40391665+SinanEmiroglu@users.noreply.github.com>
2020-06-07 00:13:20 +0300

    Added DataBaseManagement
    Added Plugins
    Added DatabaseConnector
    Added StudentData, SupervisorData, TopicData

---
+++ Assets/Scripts/DBManagement/DatabaseConnector.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Data.SQLite;
+
+public class DatabaseConnector
+{
+    string connector = $"URI=file:{ Application.dataPath }/MethodicaDB.sqlite";
+    ~DatabaseConnector() { }
+    public void UpdateTopicData(int studentID)
+    {
+        using (SQLiteConnection connection = new SQLiteConnection(connector))
+        {
+            connection.Open();
+
+            using (SQLiteCommand command = connection.CreateCommand())
+            {
+                string query = $"UPDATE Topic SET Student_ID = {studentID}";
+                command.CommandText = query;
+                command.ExecuteScalar();
+                connection.Close();
+            }
+        }
+    }
+
+    public SQLiteDataReader GetTableReader(string tableName)
+    {
+        using (SQLiteConnection connection = new SQLiteConnection(connector))
+        {
+            connection.Open();
+
+            using (SQLiteCommand command = connection.CreateCommand())
+            {
+                string query = $"SELECT * FROM {tableName}";
+                command.CommandText = query;
+                return command.ExecuteReader();
+            }
+        }
+    }
+}
