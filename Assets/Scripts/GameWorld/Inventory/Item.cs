e0b55770ff713b238572536e595b4a5fe7ac1b23
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-06-15 17:40:28 +0300

    Added Assets and Prefabs for inventory

--- Assets/Scripts/GameWorld/Inventory/Item.cs
+++ Assets/Scripts/GameWorld/Inventory/Item.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// This creates a scriptable object to store data for each inventory item
 [CreateAssetMenu(fileName = "New Item",menuName = "Inventory/Item")]
 public class Item : ScriptableObject
 {
