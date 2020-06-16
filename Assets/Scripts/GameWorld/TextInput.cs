083276326985f634ac61612f0ad2abbbd77314e9
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-06-16 16:14:21 +0300

    Created script for text input
    * Created TextInput script to allow players to enter text using keyboard when prompted.
    * Later will need to allow for touchscreen text input as well.

---
+++ Assets/Scripts/GameWorld/TextInput.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextInput : MonoBehaviour
+{
+    public Text inputText;
+    public bool isTyping = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            isTyping = !isTyping;
+        }
+
+        if (isTyping)
+        {
+            foreach(char c in Input.inputString)
+            {
+                if (c == '\b' && inputText.text.Length > 0)
+                {
+                    inputText.text = inputText.text.Remove(inputText.text.Length - 1);
+                }
+                else
+                {
+                    inputText.text += c;
+                }
+            }
+        }
+    }
+}
