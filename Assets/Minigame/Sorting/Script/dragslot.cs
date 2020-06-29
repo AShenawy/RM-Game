05ba85f9a2561fbb4ce37c97bb69691c1f56b5c6
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-06-29 14:12:38 +0300

    Review

--- Assets/Minigame/Sorting/Script/dragslot.cs
+++ Assets/Minigame/Sorting/Script/dragslot.cs
@@ -4,36 +4,69 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class dragslot : MonoBehaviour, IDropHandler
+namespace SortGame
 {
-
-    int points;
-    GameObject battery;
-
-    public void OnDrop(PointerEventData eventData)
+    // class naming
+    public class DragSlot : MonoBehaviour, IDropHandler
     {
-        if (battery == null)
+        // accessor not explicitly defined in this class. inconsistent with Drag class. Need to choose style.
+        int points = 0;
+        public GameObject battery;
+
+        public Manager drag;
+
+        // box position
+        private RectTransform position;
+
+        public Sprite[] chargingPhases;
+
+
+
+        private void Start()
         {
             battery = GameObject.Find("dockstation");
+            position = GetComponent<RectTransform>();
         }
 
-        if (eventData.pointerDrag == null)
+        public void OnDrop(PointerEventData eventData)
         {
-            return;
+            // It's better to move this statement to Start method than to do the check during gameplay.
+            // Can also make the Dock Station GO a variable and assign the object in inspector.
+            if (battery == null)
+            {
+                return;
+            }
+
+            if (eventData.pointerDrag == null)
+            { 
+
+                return;
+            }
+
+            Debug.Log("Dropped");
+            // The anchoredPosition of the GO this script is on can be made into a variable
+            // Place the item inside box
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = position.anchoredPosition;
+
+            if (points < 4)
+            {
+                // since the points variable isn't initialised it will automatically start at 0.
+                // However, it's better to explicitly initialise it at the top, since this can be confusing when reading the code
+                // and only see points++ without a starting value.
+                points++;
+                // Better to replace this with Sprite array than load from Resources live. Then the if check will need to be <4 
+                //Sprite sprite = Resources.Load<Sprite>("ChargingRates/charger_" + points.ToString());
+
+                Sprite sprite = chargingPhases[points];
+
+                // Suggest replacing with battery.GetComponent<Image>().sprite for easy debug. Can see it in inspector
+                //battery.GetComponent<Image>().overrideSprite = sprite;
+                battery.GetComponent<Image>().sprite = sprite;
+            }
+
+
         }
 
-        Debug.Log("Dropped");
-        eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
-        
-        if (points < 5)
-        {
-            points++;
-            Sprite sprite = Resources.Load<Sprite>("ChargingRates/charger_" + points.ToString());
-            battery.GetComponent<Image>().overrideSprite = sprite;
-        }
-        
-       
+
     }
-
-
 }
