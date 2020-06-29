849b4325ec7a104efbbd7a1f30981d6fa66bdb7b
Itsjustkewa <55203377+Itsjustkewa@users.noreply.github.com>
2020-06-29 14:04:12 +0300

    Sorting game

---
+++ Assets/Minigame/Sorting/Script/dragg.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class dragg : MonoBehaviour, IPointerDownHandler
+, IBeginDragHandler, IEndDragHandler, IDragHandler
+   {
+
+    [SerializeField] private Canvas canvas;
+
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+
+    public void OnBeginDrag(PointerEventData eventData){
+        Debug.Log("StartDrag");
+        canvasGroup.alpha = .7f;
+        canvasGroup.blocksRaycasts = false;
+
+    }
+
+    public void OnDrag(PointerEventData eventData){
+        Debug.Log("WhileDrag");
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData){
+        Debug.Log("EndDrag");
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        Debug.Log("ClickedDragged");
+
+    }
+
+}
---
+++ Assets/Minigame/Sorting/Script/dragslot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class dragslot : MonoBehaviour, IDropHandler
+{
+
+    int points;
+    GameObject battery;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (battery == null)
+        {
+            battery = GameObject.Find("dockstation");
+        }
+
+        if (eventData.pointerDrag == null)
+        {
+            return;
+        }
+
+        Debug.Log("Dropped");
+        eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+        
+        if (points < 5)
+        {
+            points++;
+            Sprite sprite = Resources.Load<Sprite>("ChargingRates/charger_" + points.ToString());
+            battery.GetComponent<Image>().overrideSprite = sprite;
+        }
+        
+       
+    }
+
+
+}
