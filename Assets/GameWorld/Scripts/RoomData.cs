d3892cc858a5054d0c37c3a0fd4411af911d17c0
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-07-02 19:20:56 +0300

    Room Data Script Updated
    Added bool check for rooms to see if a room is a save point or not for auto-saving game progress.

--- Assets/GameWorld/Scripts/RoomData.cs
+++ Assets/GameWorld/Scripts/RoomData.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 namespace Methodyca.Core
 {
-  public class RoomData : MonoBehaviour
-  {
-      [Tooltip("Sets whether player can turn around in the room or face one direction only")]
-      public bool playerCanTurn = true;
-      [Tooltip("If player can turn around, sets how many degrees a single turn does. Should be 0, 90, or 180")]
-      public int allowedTurnAngle;
-  }
+    public class RoomData : MonoBehaviour
+    {
+        [Tooltip("Sets whether player can turn around in the room or face one direction only. Default: True.")]
+        public bool playerCanTurn = true;
+        [Tooltip("If player can turn around, sets how many degrees a single turn does. Should be 0, 90, or 180")]
+        public int allowedTurnAngle;
+        [Tooltip("Does this room count as a checkpoint to save progress at? Default: False.")]
+        public bool isCheckpoint = false;
+    }
 }
