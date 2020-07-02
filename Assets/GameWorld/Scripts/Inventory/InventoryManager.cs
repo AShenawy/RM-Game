cb08bbeb75541ccd2bdc69c25e1aa3dc7c8fa1d5
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-07-02 20:47:48 +0300

    Working on Inventory

---
+++ Assets/GameWorld/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Methodyca.Core
+{
+    // This script handles the player inventory UI
+    public class InventoryManager : MonoBehaviour
+    {
+        // Make this class a singleton
+        #region Singleton
+        public static InventoryManager instance;
+        private void Awake()
+        {
+            if (instance == null)
+                instance = this;
+        }
+        #endregion
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
