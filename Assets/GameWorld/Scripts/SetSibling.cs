99383e9efd6bbeaedd480451682227f235bf01c3
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-07-16 19:01:50 +0300

    Setting up mobile interface - Mail system

---
+++ Assets/GameWorld/Scripts/SetSibling.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetSibling : MonoBehaviour
+{
+    public enum Rank {First, Last}
+
+    public Rank setRank;
+
+    public void SetRank()
+    {
+        switch(setRank)
+        {
+            case Rank.First:
+                GetComponent<RectTransform>().SetAsFirstSibling();
+                break;
+
+            case Rank.Last:
+                GetComponent<RectTransform>().SetAsLastSibling();
+                break;
+        }
+    }
+}
