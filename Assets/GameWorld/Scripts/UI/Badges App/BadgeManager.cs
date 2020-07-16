6b3e4337f83350e9fdd8d84e0e8aac39ffcfc378
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-07-17 01:16:59 +0300

    Created BadgeManager script

---
+++ Assets/GameWorld/Scripts/UI/Badges App/BadgeManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Methodyca.Core
+{
+    // This class handles badge progression and locked/unlocked badges in player phone
+    public class BadgeManager : MonoBehaviour
+    {
+        #region Singleton
+        public static BadgeManager instance;
+        private void Awake()
+        {
+            if (instance == null)
+                instance = this;
+        }
+        #endregion
+
+
+
+    }
+}
