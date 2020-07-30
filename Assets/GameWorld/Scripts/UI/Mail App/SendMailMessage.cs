ac71b353a2937248dae622ab2a8051d8fb8f576d
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-07-31 00:20:28 +0300

    Added SendMailMessage & removed AddMail scripts
    - replaced AddMail which was for debugging with new SendMailMessage script
    - The new script will be used in-game for objects that send the player messages/hints.

---
+++ Assets/GameWorld/Scripts/UI/Mail App/SendMailMessage.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Methodyca.Core
+{
+    // This script allows the game object it's on to send a message/tip to mail phone app
+    public class SendMailMessage : MonoBehaviour
+    {
+        public GameObject mailPrefab;
+
+        public void Send()
+        {
+            MailManager.instance.SendMail(mailPrefab);
+        }
+    }
+}
