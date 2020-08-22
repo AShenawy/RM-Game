7eaf75c39255d1646e7c0e9435405ff7706159cf
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-08-22 14:31:45 +0300

    Created new misc script and folder organisation
    - Created misc script SwitchImageDisplay which hides/shows/switches images on game objects
    - Removed 2 unused and redundant scripts ShowObject and HideObject. They only set object as active or inactive and are redundant, also unused in the scene.
    - Moved some scripts in new folders for file organisation

---
+++ Assets/GameWorld/Scripts/Misc/SwitchImageDisplay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+
+namespace Methodyca.Core
+{
+    public class SwitchImageDisplay : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
