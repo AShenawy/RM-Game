af34b4d2fa9bc916d6671256ee77d406ad1ac35e
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-09-13 22:06:06 +0300

    Updated WrapAround2D script
    - modified behaviour to be inline with Translate script
    - variable names changed to more understandable ones
    - Added comments

--- Assets/GameWorld/Scripts/Misc/WrapAround2D.cs
+++ Assets/GameWorld/Scripts/Misc/WrapAround2D.cs
@@ -3,16 +3,14 @@
 // This scripts teleports a game object to the opposite side of the screen once it exits it (for 2D assets)
 namespace Methodyca.Core
 {
-    [RequireComponent(typeof(SpriteRenderer))]
+    [RequireComponent(typeof(SpriteRenderer))]  // this script currently works only with SpriteRender component
     public class WrapAround2D : MonoBehaviour
     {
-        private enum EdgeDirection { Right, Left, Top, Bottom};
-        [SerializeField] private EdgeDirection wrapAfterObjectEdge;
-        public int edgeBuffer = 10;
+        private enum ExitDirection { Right, Left, Top, Bottom };
+        [SerializeField] private ExitDirection objectExitSide;      // which side of the screen will object exit it?
+        public int edgeBuffer = 10;     // how many pixels after complete exit before object is wrapped around?
 
         SpriteRenderer spriteRend;
-        Vector3 distanceOutMin;
-        Vector3 distanceOutMax;
 
         // Start is called before the first frame update
         void Start()
@@ -23,66 +21,67 @@ namespace Methodyca.Core
         // Update is called once per frame
         void Update()
         {
-            distanceOutMin = Camera.main.WorldToScreenPoint(spriteRend.bounds.min);
-            distanceOutMax = Camera.main.WorldToScreenPoint(spriteRend.bounds.max);
-
             CheckBoundaryConditions();
         }
 
         void CheckBoundaryConditions()
         {
-            switch (wrapAfterObjectEdge)
+            // get min and max edges of sprite in screen space to compare against play screen
+            Vector3 minSpriteBounds = Camera.main.WorldToScreenPoint(spriteRend.bounds.min);
+            Vector3 maxSpriteBounds = Camera.main.WorldToScreenPoint(spriteRend.bounds.max);
+
+            // if any of the edges exited the screen, then wrap the object around
+            switch (objectExitSide)
             {
-                case EdgeDirection.Left:
-                    if ((int)distanceOutMin.x - edgeBuffer > Screen.width)
+                case ExitDirection.Right:       // check the left-most point of sprite
+                    if ((int)minSpriteBounds.x - edgeBuffer > Screen.width)
                         ShiftObject();
                     break;
 
-                case EdgeDirection.Right:
-                    if ((int)distanceOutMax.x + edgeBuffer < 0)
+                case ExitDirection.Left:        // check right-most point of sprite
+                    if ((int)maxSpriteBounds.x + edgeBuffer < 0)
                         ShiftObject();
                     break;
 
-                case EdgeDirection.Top:
-                    if ((int)distanceOutMax.y + edgeBuffer < 0)
+                case ExitDirection.Bottom:      // check lowest point of sprite
+                    if ((int)maxSpriteBounds.y + edgeBuffer < 0)
                         ShiftObject();
                     break;
 
-                case EdgeDirection.Bottom:
-                    if ((int)distanceOutMin.y - edgeBuffer > Screen.height)
+                case ExitDirection.Top:         // check highest point of sprite
+                    if ((int)minSpriteBounds.y - edgeBuffer > Screen.height)
                         ShiftObject();
                     break;
             }
-
         }
 
         void ShiftObject()
         {
             float randomizer = Random.Range(0f, 3f);
 
-            switch (wrapAfterObjectEdge)
+            switch (objectExitSide)
             {
-                case EdgeDirection.Left:
-                    Vector3 leftEdge = Camera.main.ScreenToWorldPoint(new Vector3(-edgeBuffer, 0, 0));
-                    float leftShift = leftEdge.x - (spriteRend.bounds.size.x / 2);
+                case ExitDirection.Right:
+                    Vector3 screenLeftEdge = Camera.main.ScreenToWorldPoint(new Vector3(-edgeBuffer, 0, 0));
+                    float leftShift = screenLeftEdge.x - (spriteRend.bounds.size.x / 2);
                     transform.position = new Vector3 (leftShift - randomizer, transform.position.y, transform.position.z);
                     break;
 
-                case EdgeDirection.Right:
-                    Vector3 rightEdge = Camera.main.ScreenToWorldPoint(new Vector3((Screen.width + edgeBuffer), 0, 0));
-                    float rightShift = rightEdge.x + (spriteRend.bounds.size.x / 2);
+                case ExitDirection.Left:
+                    Vector3 screenRightEdge = Camera.main.ScreenToWorldPoint(new Vector3((Screen.width + edgeBuffer), 0, 0));
+                    float rightShift = screenRightEdge.x + (spriteRend.bounds.size.x / 2);
                     transform.position = new Vector3(rightShift + randomizer, transform.position.y, transform.position.z);
                     break;
 
-                case EdgeDirection.Top:
-                    Vector3 topEdge = Camera.main.ScreenToWorldPoint(new Vector3(0, (Screen.height + edgeBuffer), 0));
-                    float topShift = topEdge.y + (spriteRend.bounds.size.y / 2);
+                case ExitDirection.Bottom:
+                    Vector3 screenTopEdge = Camera.main.ScreenToWorldPoint(new Vector3(0, (Screen.height + edgeBuffer), 0));
+                    float topShift = screenTopEdge.y + (spriteRend.bounds.size.y / 2);
                     transform.position = new Vector3(transform.position.x, topShift + randomizer, transform.position.z);
                     break;
 
-                case EdgeDirection.Bottom:
-                    Vector3 bottomEdge = Camera.main.ScreenToWorldPoint(new Vector3(0, -edgeBuffer, 0));
-                    float bottomShift = bottomEdge.y - (spriteRend.bounds.size.y / 2);
+                case ExitDirection.Top:
+                    Vector3 screenBottomEdge = Camera.main.ScreenToWorldPoint(new Vector3(0, -edgeBuffer, 0));
+                    float bottomShift = screenBottomEdge.y - (spriteRend.bounds.size.y / 2);
                     transform.position = new Vector3(transform.position.x, bottomShift - randomizer, transform.position.z);
                     break;
             }
