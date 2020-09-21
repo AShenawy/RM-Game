2194b8f62443682512ca7c7c961e7c8003d92546
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-09-21 03:34:21 +0300

    General Setup
    - Fixed layout of canvas elements, pivots and anchors
    - Added logos for standalone minigame about screen
    - Removed unused art assets and organised folder hierarchy

---
+++ Assets/Minigames/Sorting/Script/RemoveInStandAlone.cs
@@ -0,0 +1,23 @@
+﻿#define StandAlone
+using UnityEngine;
+
+namespace Methodyca.Minigames
+{
+    // this script destroys game object if minigame will be built as standalone not in main game
+    public class RemoveInStandAlone : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Awake()
+        {
+#if StandAlone
+            Destroy(gameObject);
+#endif
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
