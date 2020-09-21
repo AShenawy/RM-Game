5f92d14cbf590113621ae15223bd8e37fc6335fd
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-09-21 04:28:08 +0300

    changed enlarged item display functionality
    - Removed inactive game objects in scene for front hi-res display
    - Created prefabs for all 15 front displays
    - Created and added new script (DisplayEnlarged) to all on-desk items, which instantiates a prefab with the enlarged view of the relevant object clicked
    - Created and added new script (Destroy Object) which destroys the instantiated prefab on click to continue with the game

---
+++ Assets/Minigames/Sorting/Script/DestroyObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Methodyca.Minigames.SortGame
+{
+    public class DestroyObject : MonoBehaviour
+    {
+        public GameObject objectToDestroy;
+
+        public void DestroyGameObject()
+        {
+            Destroy(objectToDestroy);
+        }
+    }
+}
---
+++ Assets/Minigames/Sorting/Script/DisplayEnlarged.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Methodyca.Minigames.SortGame
+{
+    // this script handles instantiating the front view larger image prefab
+    public class DisplayEnlarged : MonoBehaviour
+    {
+        public GameObject enlargedPrefab;
+
+        public void InstantiateEnlarged()
+        {
+            Instantiate(enlargedPrefab);
+        }
+    }
+}
