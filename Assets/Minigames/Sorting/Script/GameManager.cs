6902eb3dcd7995eac52403b9ae18e3b862dad3d3
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-09-21 09:43:02 +0300

    updated GameManager
    level complete variable renamed to win screen and same for game object

--- Assets/Minigames/Sorting/Script/GameManager.cs
+++ Assets/Minigames/Sorting/Script/GameManager.cs
@@ -8,32 +8,27 @@ namespace Methodyca.Minigames.SortGame
     public class GameManager : MonoBehaviour
     {
         public bool filesArranged, completed = false;
-        public GameObject filesSorted;
+        public GameObject winScreen;
         SoundManager soundMan;
         
         
         void Start()
         {
             soundMan = FindObjectOfType<SoundManager>();
-
         }
+
         public void Complete()
         {
-            if(!completed)
+            if(!completed)  // ******************* is check necessary?
             {
                 completed = true;
-                filesSorted.SetActive(true);
+                winScreen.SetActive(true);
                 soundMan.Stop("Fraud Full");
                 soundMan.Stop("static");
                 soundMan.Play("comp");
                 soundMan.Play("staticL");
             }
         }
-        void Update()
-        {
-            //Complete();
-        }
-      
     }
 
 }
