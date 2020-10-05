2e16af76f729af50726772e4279b7195056cf837
Alawode 'Fiyikewa <55203377+Itsjustkewa@users.noreply.github.com>
2020-10-05 18:49:32 +0300

    Added Sound Manager

--- Assets/GameWorld/Scripts/Interactables/Observe.cs
+++ Assets/GameWorld/Scripts/Interactables/Observe.cs
@@ -7,9 +7,12 @@ public class Observe : ObjectInteraction
     [Header("Specific Observe Paramters")]
     [TextArea, Tooltip("Dialogue to display when interacting with object")]
     public string[] inspectDialogue;
+    public Sound OberservSFX;
+    
 
     public override void InteractWithObject()
     {
         DialogueHandler.instance.DisplayDialogue(inspectDialogue);
+        SoundManager.instance.PlaySFX(OberservSFX);
     }
 }
