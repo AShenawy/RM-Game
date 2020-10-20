d77e9f99b199f6fff6f50f91046f34903c40f8f9
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-10-20 18:37:55 +0300

    Updated this branch from Gameplay-world_general branch to latest

--- Assets/GameWorld/Scripts/Sound/Sound.cs
+++ Assets/GameWorld/Scripts/Sound/Sound.cs
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-﻿using UnityEngine.Audio;
-using UnityEngine;
-
-namespace Methodyca.Core
-{
-  
-        [System.Serializable]
-        public class Sound
-        {
-            public string name;
-            public AudioClip clip;
-            public bool loop;
-
-            [Range(0f, 1f)]
-            public float volume;
-
-            [Range(.1f, 3f)]
-            public float pitch = 1f;
-
-            [Range(-1f, 1f)]
-            public float pan;
-
-            [HideInInspector]
-            public AudioSource Source;
-        }
-    
-=======
 ﻿using UnityEngine;
 
 namespace Methodyca.Core
@@ -51,5 +23,4 @@ namespace Methodyca.Core
         [HideInInspector]
         public AudioSource source;  //audio source of the sounds
     }   
->>>>>>> 98d7a18d1e6b0b4d16c16883d8cfe0758e26c969
 }
--- Assets/GameWorld/Scripts/Sound/SoundManager.cs
+++ Assets/GameWorld/Scripts/Sound/SoundManager.cs
@@ -1,26 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< HEAD
-using System;
-
-namespace Methodyca.Core
-{
-    public sealed class SoundManager : MonoBehaviour
-    {
-        public static SoundManager instance;
-        private AudioSource BGMPlayer;
-        private List<Sound> SFXPlayers = new List<Sound>();
-
-        private void Awake()
-        {
-            if (instance == null)
-                instance = this;
-            BGMPlayer = gameObject.AddComponent<AudioSource>();
-        }
-
-        private void Start()
-=======
 
 
 namespace Methodyca.Core
@@ -44,7 +24,6 @@ namespace Methodyca.Core
         }
 
         void Start()
->>>>>>> 98d7a18d1e6b0b4d16c16883d8cfe0758e26c969
         {
             StartCoroutine(CheckEndedSFX());
         }
@@ -52,40 +31,13 @@ namespace Methodyca.Core
         public void PlayBGM(Sound soundClip)
         {
             BGMPlayer.clip = soundClip.clip;
-<<<<<<< HEAD
-            BGMPlayer.volume = soundClip.volume;
-=======
             BGMPlayer.volume = soundClip.amp;
->>>>>>> 98d7a18d1e6b0b4d16c16883d8cfe0758e26c969
             BGMPlayer.pitch = soundClip.pitch;
             BGMPlayer.loop = soundClip.loop;
             BGMPlayer.panStereo = soundClip.pan;
             BGMPlayer.Play();
         }
 
-<<<<<<< HEAD
-        public void PlaySFX(Sound sfxClip, bool doMouseImaging = false)
-        {
-            if (SFXPlayers.Exists(x => x.name == sfxClip.name))
-            {
-                SFXPlayers.Find(x => x.name == sfxClip.name).Source.Play();
-                return;
-            }
-            SFXPlayers.Add(sfxClip);
-            sfxClip.Source = gameObject.AddComponent<AudioSource>();
-            sfxClip.Source.clip = sfxClip.clip;
-            sfxClip.Source.volume = sfxClip.volume;
-            sfxClip.Source.pitch = sfxClip.pitch;
-            sfxClip.Source.loop = sfxClip.loop;
-
-            if (sfxClip.pan != 0)
-                sfxClip.Source.panStereo = sfxClip.pan;
-            else if (doMouseImaging)
-                sfxClip.Source.panStereo = sfxClip.pan;
-
-            sfxClip.Source.Play();
-
-=======
         public void PlaySFX(Sound sfxCLip, bool doMouseImaging = false)
         {
             // if the SFX is already playing then just reset it and play from the start
@@ -110,19 +62,14 @@ namespace Methodyca.Core
                 sfxCLip.source.panStereo = SFXImaging();
 
             sfxCLip.source.Play();
->>>>>>> 98d7a18d1e6b0b4d16c16883d8cfe0758e26c969
         }
 
         private float SFXImaging()
         {
-<<<<<<< HEAD
-            float mouseX = Input.mousePosition.x;
-=======
             // get the horizontal component of the mouse
             float mouseX = Input.mousePosition.x;
 
             // compare the ratio between mouse horizontal position relative to the screen, then lerp it between range of values for stereo pan
->>>>>>> 98d7a18d1e6b0b4d16c16883d8cfe0758e26c969
             float panner = Mathf.Lerp(-1, 1, mouseX / Screen.width);
             return panner;
         }
@@ -137,14 +84,6 @@ namespace Methodyca.Core
             if (SFXPlayers.Exists(x => x.name == clipName))
             {
                 Sound SFXSound = SFXPlayers.Find(x => x.name == clipName);
-<<<<<<< HEAD
-                SFXSound.Source.Stop();
-                SFXPlayers.Remove(SFXSound);
-                Destroy(SFXSound.Source);
-            }
-        }
-
-=======
                 SFXSound.source.Stop();
                 SFXPlayers.Remove(SFXSound);
                 Destroy(SFXSound.source);
@@ -152,20 +91,10 @@ namespace Methodyca.Core
         }
 
         // Check every 0.2 seconds if any SFX stopped playing (besides looping). If yes, then kill it 
->>>>>>> 98d7a18d1e6b0b4d16c16883d8cfe0758e26c969
         IEnumerator CheckEndedSFX()
         {
             foreach (Sound sfx in SFXPlayers.ToArray())
             {
-<<<<<<< HEAD
-                if (sfx.Source.isPlaying == false)
-                {
-                    SFXPlayers.Remove(sfx);
-                    Destroy(sfx.Source);
-                }
-            }
-            yield return new WaitForSeconds(0.25f);
-=======
                 if (sfx.source.isPlaying == false)
                 {
                     SFXPlayers.Remove(sfx);
@@ -174,35 +103,22 @@ namespace Methodyca.Core
             }
 
             yield return new WaitForSeconds(0.2f);
->>>>>>> 98d7a18d1e6b0b4d16c16883d8cfe0758e26c969
             StartCoroutine(CheckEndedSFX());
         }
 
         public void ChangeAllSFXVolume(float volume)
         {
-<<<<<<< HEAD
-            SFXPlayers.ForEach(s => s.Source.volume = Mathf.Clamp01(volume));
-=======
             SFXPlayers.ForEach(s => s.source.volume = Mathf.Clamp01(volume));
->>>>>>> 98d7a18d1e6b0b4d16c16883d8cfe0758e26c969
         }
 
         public void StopAllSFX()
         {
             foreach (Sound sfx in SFXPlayers.ToArray())
             {
-<<<<<<< HEAD
-                Destroy(sfx.Source);
-                SFXPlayers.Remove(sfx);
-            }
-        }
-    }
-=======
                 Destroy(sfx.source);
                 SFXPlayers.Remove(sfx);
             }
         }
     } 
->>>>>>> 98d7a18d1e6b0b4d16c16883d8cfe0758e26c969
 }
 
