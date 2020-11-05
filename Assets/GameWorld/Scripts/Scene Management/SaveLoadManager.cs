cbebdf6d07db53ece1a998d16cd1c5dbb7d109de
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-11-05 15:36:48 +0200

    first step in saving and loading current room

---
+++ Assets/GameWorld/Scripts/Scene Management/SaveLoadManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveLoadManager : MonoBehaviour
+{
+    public static SaveLoadManager instance;
+
+    #region Singleton
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+    #endregion
+
+    public void SaveJSON(string key, Object saveObj)
+    {
+        PlayerPrefs.SetString(key, JsonUtility.ToJson(saveObj));
+    }
+
+    public void SaveJSON(string key, PlayerData saveObj)
+    {
+        PlayerPrefs.SetString(key, JsonUtility.ToJson(saveObj));
+    }
+
+    public void LoadJSON(string key, Object loadObj)
+    {
+        JsonUtility.FromJsonOverwrite(PlayerPrefs.GetString(key), loadObj);
+    }
+
+    public void LoadJSON(string key, PlayerData loadObj)
+    {
+        JsonUtility.FromJsonOverwrite(PlayerPrefs.GetString(key), loadObj);
+    }
+
+    public PlayerData LoadJSON(string key)
+    {
+        return JsonUtility.FromJson<PlayerData>(PlayerPrefs.GetString(key));
+    }
+}
