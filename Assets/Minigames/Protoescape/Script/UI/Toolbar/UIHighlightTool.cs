78f6dd7fb1f3a176640cbaab6f5adce98dce1e32
SinanEmiroglu <40391665+SinanEmiroglu@users.noreply.github.com>
2020-11-10 12:20:02 +0200

    Updated Data, Locations & Added Some Toolbar Icons

--- Assets/Minigames/Protoescape/Script/UI/Toolbar/UIHighlightTool.cs
+++ Assets/Minigames/Protoescape/Script/UI/Toolbar/UIHighlightTool.cs
@@ -5,9 +5,6 @@ namespace Methodyca.Minigames.Protoescape
 {
     public class UIHighlightTool : UIBaseTool, IPointerClickHandler
     {
-        [SerializeField] private Sprite on;
-        [SerializeField] private Sprite off;
-
         private IHighlightable _selection;
 
         public void OnPointerClick(PointerEventData eventData)
@@ -20,21 +17,9 @@ namespace Methodyca.Minigames.Protoescape
             if (selection != null && selection.GetComponent<IHighlightable>() != null)
             {
                 Enable();
-
-                _selection = selection.GetComponent<IHighlightable>();
-
-                if (_selection.IsHighlighted)
-                {
-                    toolbarIcon.sprite = on;
-                }
-                else
-                {
-                    toolbarIcon.sprite = off;
-                }
             }
             else
             {
-                toolbarIcon.sprite = off;
                 Disable();
             }
         }
