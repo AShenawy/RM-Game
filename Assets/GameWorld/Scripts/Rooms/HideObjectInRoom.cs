f2670de3f04c7fa3c9ba78a41214f066d0d3ade2
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-11-22 01:42:28 +0200

    Update HideObjectInRoom.cs
    Hide Objects script now saves which spot an object is hidden in for game saving/loading

--- Assets/GameWorld/Scripts/Rooms/HideObjectInRoom.cs
+++ Assets/GameWorld/Scripts/Rooms/HideObjectInRoom.cs
@@ -3,17 +3,34 @@
 
 namespace Methodyca.Core
 {
-    public class HideObjectInRoom : MonoBehaviour
+    // script added to room game objects for hiding objects in random locations
+    public class HideObjectInRoom : MonoBehaviour, ILoadable
     {
         public GameObject objectToHide;
         public GameObject[] hidingSpots;
-
-        // Start is called before the first frame update
+        private int spotNumber = -1;
+        
         void Start()
         {
-            int random = Random.Range(0, hidingSpots.Length);
+            LoadState();
+            if (spotNumber == -1)   // if LoadState() returned nothing then assign a new value
+                spotNumber = Random.Range(0, hidingSpots.Length);
 
-            Instantiate(objectToHide, hidingSpots[random].transform);
+            Instantiate(objectToHide, hidingSpots[spotNumber].transform);
+            SaveState(spotNumber);
+        }
+
+        public void LoadState()
+        {
+            // don't use TryGetValue as it returns 0 if no key is found. this negates the RNG
+            if (SaveLoadManager.interactableStates.ContainsKey(name))
+                spotNumber = SaveLoadManager.interactableStates[name];
+        }
+
+        // saving the state without implementing ISaveable interface due to method argument
+        public void SaveState(int value)
+        {
+            SaveLoadManager.SetInteractableState(name, value);
         }
     }
 }
