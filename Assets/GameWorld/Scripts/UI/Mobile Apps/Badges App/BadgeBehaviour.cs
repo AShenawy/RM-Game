235a68d4f916421dab1fef35b84e0b984b56f7f3
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2020-11-22 19:38:01 +0200

    Creating Badge Manager, Badge System, and cleanup..
    removed older UI graphics which aren't used anymore

---
+++ Assets/GameWorld/Scripts/UI/Mobile Apps/Badges App/BadgeBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BadgeBehaviour : MonoBehaviour
+{
+    private Text badgeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        badgeText = GetComponentInChildren<Text>();
+    }
+
+    public void UpdateBadgeInfo()
+    {
+
+    }
+}
