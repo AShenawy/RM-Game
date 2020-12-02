606fc12d1b14146ce50f0612a79db764b3a50d12
SinanEmiroglu <40391665+SinanEmiroglu@users.noreply.github.com>
2020-12-02 22:11:33 +0200

    Added Sprites

---
+++ Assets/Minigames/Protoescape/Script/Mover.cs
@@ -0,0 +1,52 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Methodyca.Minigames.Protoescape
+{
+    public class Mover : MonoBehaviour
+    {
+        [SerializeField] private bool isPlayOnStart;
+        [SerializeField] private float duration;
+        [SerializeField] private Transform endPoint;
+
+        private Tween _moveTween;
+        private Transform _transform;
+
+        public void PlayPauseMoveTween()
+        {
+            _moveTween.TogglePause();
+        }
+
+        private void Awake()
+        {
+            _transform = transform;
+        }
+
+        private void Start()
+        {
+            float _duration = Mathf.Abs(duration);
+            Vector2 startPosition = _transform.position;
+
+            _moveTween = DOTween.Sequence().AppendCallback(() => _transform.LookAt(endPoint, Vector3.up))
+                                           .Append(_transform.DOMove(endPoint.position, _duration))
+                                           .AppendCallback(() => _transform.LookAt(startPosition, Vector3.up))
+                                           .Append(_transform.DOMove(startPosition, _duration))
+                                           .SetEase(Ease.Linear).SetLoops(-1).SetAutoKill(false).Pause();
+
+            if (isPlayOnStart)
+            {
+                PlayPauseMoveTween();
+            }
+        }
+
+        private void OnDisable()
+        {
+            _moveTween.Pause();
+        }
+
+        private void OnDestroy()
+        {
+            _moveTween.Kill();
+        }
+    }
+}
---
+++ Assets/Minigames/Protoescape/Script/UI/UIIntro.cs
@@ -0,0 +1,15 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Methodyca.Minigames.Protoescape
+{
+    public class UIIntro : MonoBehaviour
+    {
+       // [SerializeField] private RectTransform alien;
+
+        private void Start()
+        {
+          //  alien.DOShakePosition(6, 300, 1, fadeOut: false).OnComplete(() => GameManager_Protoescape.Instance.HandleGameStart());
+        }
+    }
+}
