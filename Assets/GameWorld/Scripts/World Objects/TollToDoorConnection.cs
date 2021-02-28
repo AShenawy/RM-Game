7add450a6cebeeaba8424608f42b67ee72d7fb96
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2021-02-28 23:12:24 +0200

    Updated Act2 N1
    new script to connect N1 doors unlocking to toll machines

---
+++ Assets/GameWorld/Scripts/World Objects/TollToDoorConnection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Methodyca.Core
+{
+    // this script connects the toll machine to the door for unlocking it
+    public class TollToDoorConnection : MonoBehaviour
+    {
+        [SerializeField] private Door door;
+
+        private void OnEnable()
+        {
+            GetComponent<Operate>().onOperation += UnlockConnectedDoor;
+        }
+
+        void UnlockConnectedDoor(Operate opr)
+        {
+            door.Unlock();
+        }
+
+        private void OnDisable()
+        {
+            GetComponent<Operate>().onOperation -= UnlockConnectedDoor;
+        }
+    }
+}
