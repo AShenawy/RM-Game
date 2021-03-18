ff0c41355518eaf080ddec3131655bfd628f4da6
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2021-03-18 17:09:08 +0200

    General updates

--- Assets/GameWorld/Scripts/Interactables/LinkInteractables.cs
+++ Assets/GameWorld/Scripts/Interactables/LinkInteractables.cs
@@ -3,7 +3,8 @@
 
 namespace Methodyca.Core
 {
-    // this script links 2 interactable objects for loading the game states if not in same room
+    // this script links 2 interactable objects, forcing the linked object to load its state
+    // before becoming active and running Start() on itself
     public class LinkInteractables : MonoBehaviour
     {
         public ObjectInteraction linkedInteractableObject;
