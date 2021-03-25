bb2b7614c3c5878802cd7c03dc1b9cec17bb4787
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2021-03-25 22:01:06 +0200

    Update BadgeManager.cs
    added new N2 minigames to be completed via ink stories

--- Assets/GameWorld/Scripts/Badges/BadgeManager.cs
+++ Assets/GameWorld/Scripts/Badges/BadgeManager.cs
@@ -46,6 +46,12 @@ namespace Methodyca.Core
                 SetMinigameComplete((int)Minigames.Observation);
             if (Input.GetKeyDown(KeyCode.Alpha9))
                 SetMinigameComplete((int)Minigames.Persona);
+            if (Input.GetKeyDown(KeyCode.I))
+                SetMinigameComplete((int)Minigames.Survey);
+            if (Input.GetKeyDown(KeyCode.O))
+                SetMinigameComplete((int)Minigames.CaseStudy);
+            if (Input.GetKeyDown(KeyCode.P))
+                SetMinigameComplete((int)Minigames.DesignResearch);
 #endif
         }
 
@@ -86,5 +92,6 @@ namespace Methodyca.Core
     }
 
     // a list of all available minigames
-    public enum Minigames { Blank, Sorting, DocStudy, Participatory, Prototyping, Questionnaire, Research, Interview, Observation, Persona }
+    public enum Minigames { Blank, Sorting, DocStudy, Participatory, Prototyping, Questionnaire,
+                            Research, Interview, Observation, Persona, Survey, CaseStudy, DesignResearch }
 }
