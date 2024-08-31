1932c418f7546149e4b5d9df6f339c04cefdec18
nikdol25 <dolgovnikita1@gmail.com>
2024-08-31 22:29:38 +0300

    Added sound ON/OFF button. Updated menu.
    Added sound ON/OFF button
    Updated menu (About - Authors)

---
+++ Assets/Minigames/Questioniser/Scripts/Sound/MusicController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicController : MonoBehaviour
+{
+    public Sprite offSprite;
+    public Sprite onSprite;
+
+    private Image buttonImage;
+    private bool isMuted = false;
+
+    void Start()
+    {
+        buttonImage = GetComponent<Image>();
+        UpdateButtonSprite();
+    }
+
+    public void ToggleMusic()
+    {
+        // Toggle the mute state
+        isMuted = !isMuted;
+
+        // Set the global volume to 0 if muted, 1 if unmuted
+        AudioListener.volume = isMuted ? 0 : 1;
+
+        // Update the button sprite based on the current state
+        UpdateButtonSprite();
+    }
+
+    private void UpdateButtonSprite()
+    {
+        if (buttonImage != null)
+        {
+            buttonImage.sprite = isMuted ? offSprite : onSprite;
+        }
+    }
+}
