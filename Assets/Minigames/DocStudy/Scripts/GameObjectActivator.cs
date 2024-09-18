635827f45cd6340e0950c3655beb4fdcc95e7fbf
Ekiz24 <tkboxnn@gmail.com>
2024-09-18 22:30:46 +0300

    Modified Back Button
    Added the script GameObjectActivator, which is mainly used to detect if the mounted game object is active or not, and to hide the back button.

--- Assets/Minigames/DocStudy/Scripts/GameObjectActivator.cs
+++ Assets/Minigames/DocStudy/Scripts/GameObjectActivator.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameObjectActivator : MonoBehaviour
 {
     public static event System.Action OnGameObjectActivated = delegate { };
     public static event System.Action OnGameObjectDeactivated = delegate { };
+    [SerializeField] private Button previousButton;
+
+    private void Start()
+    {
+        if (previousButton!=null)
+        {
+            previousButton.gameObject.SetActive(false);
+        }
+    }
 
     private void OnEnable()
     {
