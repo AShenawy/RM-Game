4730459f975adbdb55c62ed889f0e0169908d38d
Ekiz24 <tkboxnn@gmail.com>
2024-11-08 22:33:25 +0200

    Deleted Typing Effects
    1. Deleted sound effects when playing dialog
    2. Deleted typing effects of texts

--- Assets/Minigames/DocStudy/Scripts/UI/UIDialog.cs
+++ Assets/Minigames/DocStudy/Scripts/UI/UIDialog.cs
@@ -13,19 +13,8 @@ namespace Methodyca.Minigames.DocStudy
         [SerializeField] private TextMeshProUGUI speech;
         [SerializeField] private Button nextButton;
         [SerializeField] private Button previousButton;
-        [SerializeField] private AudioClip typeSoundClip; // AudioClip for the typewriter sound
-        [SerializeField] private float typingSpeed = 0.05f; // Delay between each character
         [SerializeField] private Sprite nextIcon;
         [SerializeField] private Sprite endIcon;
-
-        private AudioSource audioSource; // We'll create this at runtime
-        private Coroutine typingCoroutine;
-
-        private void Awake()
-        {
-            // Add or find the AudioSource component on this GameObject
-            audioSource = gameObject.AddComponent<AudioSource>();
-        }
 
         private void OnEnable()
         {
@@ -43,27 +32,12 @@ namespace Methodyca.Minigames.DocStudy
                 return;
 
             character.sprite = dialog.Character;
+            speech.text = dialog.Speech;
 
-            // Stop any existing typing effect
-            if (typingCoroutine != null)
-            {
-                StopCoroutine(typingCoroutine);
-            }
 
             // Start the fade-in and then the typing effect
             DOTween.Sequence().AppendCallback(() => canvasGroup.alpha = 0)
-                              .Append(DOTween.To(() => canvasGroup.alpha, a => canvasGroup.alpha = a, 1, 0.25f))
-                              .OnComplete(() =>
-                              {
-                                  if (dialog != null && gameObject.activeInHierarchy)
-                                  {
-                                      typingCoroutine = StartCoroutine(TypeText(dialog.Speech));
-                                  }
-                                  else
-                                  {
-                                      Debug.LogWarning("Cannot start typing coroutine because the game object is inactive.");
-                                  }
-                              });
+                              .Append(DOTween.To(() => canvasGroup.alpha, a => canvasGroup.alpha = a, 1, 0.25f));
 
             if (DialogManager.Instance.HasPreviousDialog())
             {
@@ -85,51 +59,17 @@ namespace Methodyca.Minigames.DocStudy
             }
         }
 
-        private IEnumerator TypeText(string text)
-        {
-            speech.text = "";
-            foreach (char letter in text)
-            {
-                speech.text += letter;
-
-                if (typeSoundClip != null && audioSource != null)
-                {
-                    audioSource.PlayOneShot(typeSoundClip); // Play the sound effect for each letter
-                }
-
-                yield return new WaitForSeconds(typingSpeed); // Wait between each letter
-            }
-        }
-
         private void ClickNextHandler()
         {
-            // Stop the typing effect if it's ongoing and immediately display full text
-            if (typingCoroutine != null)
-            {
-                StopCoroutine(typingCoroutine);
-                speech.text = DialogManager.Instance.GetCurrentDialog().Speech;
-            }
-
-            // Fade out the dialog before triggering the next one
             DOTween.To(() => canvasGroup.alpha, a => canvasGroup.alpha = a, 0, 0.25f)
                    .OnComplete(() =>
                    {
                        DialogManager.Instance.TriggerDialog();
                    });
-
-
         }
 
         private void ClickPreviousHandler()
         {
-
-            if (typingCoroutine != null)
-            {
-                StopCoroutine(typingCoroutine);
-                speech.text = DialogManager.Instance.GetCurrentDialog().Speech;
-            }
-
-            // Fade out the dialog before triggering the previous one
             DOTween.To(() => canvasGroup.alpha, a => canvasGroup.alpha = a, 0, 0.25f)
                    .OnComplete(() =>
                    {
