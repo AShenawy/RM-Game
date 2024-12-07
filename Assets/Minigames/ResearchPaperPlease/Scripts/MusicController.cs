2ee263a39e94f41bdd3081e51762dd2665c9ded5
Ekiz24 <tkboxnn@gmail.com>
2024-12-07 17:53:21 +0200

    Music Controller
    Changed the sound options in the setting,
    Background music and sound effects can now be disabled separately.

--- Assets/Minigames/ResearchPaperPlease/Scripts/MusicController.cs
+++ Assets/Minigames/ResearchPaperPlease/Scripts/MusicController.cs
@@ -5,14 +5,12 @@ public class MusicController : MonoBehaviour
 {
     public Sprite offSprite;
     public Sprite onSprite;
-    public AudioSource correctOrWrongAudioSource;
+    public AudioSource[] soundAudioSource;
     public AudioSource backgroundMusicAudioSource;
 
-    public Image globalMusicImage;
-    public Image correctOrWrongSoundImage;
+    public Image SoundImage;
     public Image backgroundMusicImage;
-    private bool isMuted = false;
-    private bool isCorrectOrWrongMuted = false;
+    private bool isSoundMuted = false;
 
     private bool isBackgroundMusicMuted = false;
     void Start()
@@ -20,27 +18,29 @@ public class MusicController : MonoBehaviour
         UpdateButtonSprite();
     }
 
-    public void ToggleGlobalMusic()
-    {
-        // Toggle the global mute state
-        isMuted = !isMuted;
+    // public void ToggleGlobalMusic()
+    // {
+    //     // Toggle the global mute state
+    //     isMuted = !isMuted;
 
-        // Set the global volume to 0 if muted, 1 if unmuted
-        AudioListener.volume = isMuted ? 0 : 1;
+    //     // Set the global volume to 0 if muted, 1 if unmuted
+    //     AudioListener.volume = isMuted ? 0 : 1;
 
-        // Update the button sprite based on the current state
-        UpdateButtonSprite();
-    }
+    //     // Update the button sprite based on the current state
+    //     UpdateButtonSprite();
+    // }
 
     public void ToggleTargetMusic()
     {
-        isCorrectOrWrongMuted = !isCorrectOrWrongMuted;
+        isSoundMuted = !isSoundMuted;
 
-        if (correctOrWrongAudioSource != null)
+        if (soundAudioSource != null)
         {
-            correctOrWrongAudioSource.mute = isCorrectOrWrongMuted;
+            for (int i = 0; i < soundAudioSource.Length; i++)
+            {
+                soundAudioSource[i].mute = isSoundMuted;
+            }
         }
-
         UpdateButtonSprite();
     }
 
@@ -52,19 +52,14 @@ public class MusicController : MonoBehaviour
         {
             backgroundMusicAudioSource.mute = !backgroundMusicAudioSource.mute;
         }
-
         UpdateButtonSprite();
     }
 
     private void UpdateButtonSprite()
     {
-        if (globalMusicImage != null)
+        if (SoundImage != null)
         {
-            globalMusicImage.sprite = isMuted ? offSprite : onSprite;
-        }
-        if (correctOrWrongSoundImage != null)
-        {
-            correctOrWrongSoundImage.sprite = isCorrectOrWrongMuted ? offSprite : onSprite;
+            SoundImage.sprite = isSoundMuted ? offSprite : onSprite;
         }
         if (backgroundMusicImage != null)
         {
